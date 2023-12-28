bc4448c27250cc7c6582c6e9895f6b5084cdf07d
Jakub Rozek <jakub.rozek314@gmail.com>
2023-12-28 15:12:23 +0100

    Remove unnecessary sound-cue queue in DynamicConverter

--- owl/converters/dynamic/base.py
+++ owl/converters/dynamic/base.py
@@ -17,10 +17,9 @@ class DynamicConverter(BaseConverter):
     """Generates multiple different tones per frame"""
 
     ms_per_frame: int  # actual ms per frame will be more if sound cue provided
-    sound_cue: list[float] | None = None
+    sound_cue: Signal | None = None
     ms_between_new_frames: float = 1000 / 30  # 30 FPS is a reasonable assumption
 
-    _sound_cue_queue: list[float] = field(default_factory=list, init=False)
     _audio_samples_queue: deque[float] = field(default_factory=deque, init=False)
 
     def __post_init__(self) -> None:
@@ -35,16 +34,6 @@ class DynamicConverter(BaseConverter):
     def convert_frame(self, frame: Frame) -> Signal:
         ...
 
-    def get_next_soundgen_samples(self, count: int) -> np.ndarray:
-        def popleft_or(deq: deque, default=None):
-            if not len(deq):
-                return default
-            return deq.popleft()
-
-        return np.array(
-            [popleft_or(self._audio_samples_queue, 0.0) for _ in range(count)]
-        )
-
     def on_new_frame(self, frame: Frame) -> None:
         # do nothing if we can afford to wait for the next frame
         # three video frames should be enough time for new frame to be inserted into deque
@@ -57,7 +46,7 @@ class DynamicConverter(BaseConverter):
         # queue sound cue
         if self.sound_cue is not None:
             logger.debug("Inserting sound cue")
-            self._sound_cue_queue.extend(self.sound_cue)
+            self._audio_samples_queue.extend(self.sound_cue)
 
         queue_ms_left = 1000 * len(self._audio_samples_queue) / self.sample_rate
         logger.debug(f"Converting new video frame with {queue_ms_left:.0f}ms to spare")
@@ -65,17 +54,11 @@ class DynamicConverter(BaseConverter):
         self._audio_samples_queue.extend(self.convert_frame(frame))
 
     def get_next_samples(self, count: int) -> Signal:
-        # if requested sample count satisfied by sound cue queue, return that
-        if count <= len(self._sound_cue_queue):
-            ret = np.array(self._sound_cue_queue[:count])
-            self._sound_cue_queue = self._sound_cue_queue[count:]
-            return ret
+        def popleft_or(deq: deque, default=None):
+            if not len(deq):
+                return default
+            return deq.popleft()
 
-        # else return rest of sound cue queue and also part of the actual frame
-        remaining_sample_count = count - len(self._sound_cue_queue)
-        ret = np.array(self._sound_cue_queue)
-        self._sound_cue_queue.clear()
-
-        return np.concatenate(
-            [ret, self.get_next_soundgen_samples(remaining_sample_count)]
+        return np.array(
+            [popleft_or(self._audio_samples_queue, 0.0) for _ in range(count)]
         )
