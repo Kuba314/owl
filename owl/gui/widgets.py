bfec38a5951ead8453ce837025e1af4f2c745f48
Kuba314 <35199722+Kuba314@users.noreply.github.com>
2024-10-20 15:12:06 +0200

    Implement side-panel option switching

--- owl/gui/widgets.py
+++ owl/gui/widgets.py
@@ -1,15 +1,60 @@
-from PyQt6.QtWidgets import QGridLayout, QHBoxLayout, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import (
+    QComboBox,
+    QGridLayout,
+    QHBoxLayout,
+    QLabel,
+    QStackedWidget,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class CurveConverterOptions(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("curve option 1"))
+        layout.addWidget(QLabel("curve option 2"))
+        self.setLayout(layout)
+
+
+class DynamicConverterOptions(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel("dynamic option 1"))
+        layout.addWidget(QLabel("dynamic option 2"))
+        self.setLayout(layout)
 
 
 class SidePanel(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("side panel 1"))
-        layout.addWidget(QLabel("side panel 2"))
-        layout.addWidget(QLabel("side panel 3"))
+        self._converter_options_widgets = [
+            ("curve", CurveConverterOptions()),
+            ("dynamic", DynamicConverterOptions()),
+        ]
 
+        # initialize stack
+        self._options_stack = QStackedWidget()
+        for _, options_widget in self._converter_options_widgets:
+            self._options_stack.addWidget(options_widget)
+
+        # initialize combo box
+        self._converter_select = QComboBox()
+        for i, (name, _) in enumerate(self._converter_options_widgets):
+            self._converter_select.insertItem(i, name)
+
+        # connect and emit select signal
+        self._converter_select.currentIndexChanged.connect(
+            lambda index: self._options_stack.setCurrentIndex(index)
+        )
+        self._converter_select.currentIndexChanged.emit(0)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._converter_select)
+        layout.addWidget(self._options_stack)
         self.setLayout(layout)
 
 
